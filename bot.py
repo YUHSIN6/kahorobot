5d8403cf796992e299ce125b74032c54241ed209
6uc <clingyattic8184@gmail.com>
2024-07-11 03:16:10 +0900

    cogs initial

--- bot.py
+++ bot.py
@@ -1,29 +1,50 @@
-# 導入 套件
-import discord
 import os
+import asyncio
+import discord
+from discord.ext import commands
 from dotenv import load_dotenv
 
-# 取得環境設定
 load_dotenv()
 DISCORD_TOKEN = os.getenv("DISCORD_TOKEN")
-# intents
-intents = discord.Intents.default()
-intents.message_content = True  # NEW 新增要求讀取訊息權限 
-# client
-client = discord.Client(intents=intents)
 
-# event 事件處理
-@client.event
+intents = discord.Intents.all()
+bot = commands.Bot(command_prefix = "$", intents = intents)
+
+# 當機器人完成啟動時
+@bot.event
 async def on_ready():
-  print(f"「{client.user}」已登入")
-  
-@client.event
-async def on_message(message):
-  print(message) # 印出message 內容
-  if message.author == client.user: # 排除機器人本身的訊息
-    return
-  if message.content == 'ping':
-    await message.channel.send('pong')
+    print(f"目前登入身份 --> {bot.user}")
 
+# 載入指令程式檔案
+@bot.command()
+async def load(ctx, extension):
+    await bot.load_extension(f"cogs.{extension}")
+    await ctx.send(f"Loaded {extension} done.")
+
+# 卸載指令檔案
+@bot.command()
+async def unload(ctx, extension):
+    await bot.unload_extension(f"cogs.{extension}")
+    await ctx.send(f"UnLoaded {extension} done.")
+
+# 重新載入程式檔案
+@bot.command()
+async def reload(ctx, extension):
+    await bot.reload_extension(f"cogs.{extension}")
+    await ctx.send(f"ReLoaded {extension} done.")
+
+# 一開始bot開機需載入全部程式檔案
+async def load_extensions():
+    for filename in os.listdir("./cogs"):
+        if filename.endswith(".py"):
+            await bot.load_extension(f"cogs.{filename[:-3]}")
+
+async def main():
+    async with bot:
+        await load_extensions()
+        # 使用 DISCORD_TOKEN 來啟動 bot
+        await bot.start(DISCORD_TOKEN)
+
+# 確定執行此py檔才會執行
 if __name__ == "__main__":
-  client.run(DISCORD_TOKEN)
+    asyncio.run(main())
