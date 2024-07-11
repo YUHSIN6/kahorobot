0332fca32ab2f55c9f1debdb285cdba35be37bda
6uc <clingyattic8184@gmail.com>
2024-07-11 16:50:32 +0900

    reply完成

--- cogs/reply.py
+++ cogs/reply.py
@@ -5,12 +5,16 @@ from discord.ext import commands
 class Reply(commands.Cog):
     def __init__(self, bot: commands.Bot):
         self.bot = bot
+        self.special_user_ids = [468711293264855052]
 
     # 當有人標註機器人時回復原消息
     @commands.Cog.listener()
     async def on_message(self, message: discord.Message):
         if self.bot.user.mentioned_in(message) and not message.mention_everyone and message.author != self.bot.user:
-            await message.reply("？")
+            if message.author.id in self.special_user_ids:
+                await message.reply("怎麼了")
+            else:
+                await message.reply("?")
             #await message.channel.send(f"{message.author.mention} ？\n{message.content}")
 
 # Cog 載入 Bot 中
