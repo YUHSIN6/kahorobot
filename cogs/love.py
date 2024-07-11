1a4fac1141bf06f8a3e84f6001e08faeeb604d9a
6uc <clingyattic8184@gmail.com>
2024-07-11 12:33:22 +0900

    echo complete

--- cogs/love.py
+++ cogs/love.py
@@ -2,18 +2,17 @@ import discord
 from discord.ext import commands
 
 # 定義名為 Main 的 Cog
-class Echo(commands.Cog):
+class Love(commands.Cog):
     def __init__(self, bot: commands.Bot):
         self.bot = bot
 
     # 前綴指令
     @commands.command()
-    async def echo(self, ctx: commands.Context, *, content: str):
-        await ctx.message.delete()
-        await ctx.send(content)
+    async def love(self, ctx: commands.Context, *, content: str):
+        await ctx.send("愛してるよ")
 
 
 
 # Cog 載入 Bot 中
 async def setup(bot: commands.Bot):
-    await bot.add_cog(Echo(bot))
+    await bot.add_cog(Love(bot))
